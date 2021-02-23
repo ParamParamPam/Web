7d29366043251272ba39527c0375033cdacb8e67
Dasha <ilasovadaria@gmail.ru>
2021-02-23 20:13:18 +0300

    Update AOS_delete_tables.sql

--- db/AOS_delete_tables.sql
+++ db/AOS_delete_tables.sql
@@ -1,3 +1,2 @@
---TRUNCATE TABLE product_order, basket, "order", product, customer, "admin";
 DROP TABLE IF EXISTS product_order, basket, "order", product, customer, "admin";
 DROP TYPE IF EXISTS companies, "types", time_period, statuses;
